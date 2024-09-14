3d7d2e9d6c89c246db444fa5c40a4a6d1f4fdedb
gustavo <cru22779@uvg.edu.gt>
2024-09-14 17:13:03 -0600

    Added Gui and a simple Readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Proy1_TC
-Proyecto 1 del curso Teoría de la Computación
+# Proyecto No. 1 
+
+## Dependencias
+Para ejecutar correctamente el proyecto, es necesario que la computadora tenga instalado lo siguiente:
+    - Python 
+        - Streamlit
+        - Pip
+    - Docker (opcional)
+## ¿Cómo ejecutar el proyecto?
+Para ejecutar el proyecto de manera local, abre una terminal y ejecuta:
+```sh
+pip install Streamlit
+streamlit run ./main.py
+```
+Si no deseas instalar los paquetes de python, y tienes docker instalado puedes hacer lo siguiente:
+```sh
+docker buildx build -t proyecto1-teoria .
+docker run proyecto1-teoria
+``` 
--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-# Importar las funciones desde los archivos correspondientes
+import streamlit as st
 from shunting_yard import infix_a_postfix, agregar_concatenacion_implicita
 from Thompson import Thompson_Algorithm
+from Automata import Automata
 
+def main():
+    st.set_page_config(page_title ='Automata', page_icon="https://png.pngtree.com/png-vector/20220719/ourmid/pngtree-robot-logo-template-vector-icon-illustration-sign-symbol-computer-vector-png-image_37807128.png")
+    st.title('Construcción de Autómatas a partir de Expresiones Regulares')
+    expression = st.text_input("Ingresa la expresión regular:")
+    if st.button("Generar Autómata"):
+        if expression:
+            try:
+                expression = agregar_concatenacion_implicita(expression)
+                postfix_expression = infix_a_postfix(expression)
+                st.write(f"Expresión en notación postfix: {postfix_expression}")
 
-expression = input("Ingresa la expresión regular: ")
-expression = agregar_concatenacion_implicita(expression)
-expression = infix_a_postfix(expression)
-print(expression)
-NFA = Thompson_Algorithm(expression)
+                NFA = Thompson_Algorithm(postfix_expression)
 
-print(NFA)
+                st.write(f"Estados: {NFA.states}")
+                st.write(f"Alfabeto: {NFA.alphabet}")
+                st.write(f"Estado inicial: {NFA.initial_state}")
+                st.write(f"Estados finales: {NFA.final_states}")
+                st.write(f"Transiciones: {NFA.transitions}")
 
-print(len(NFA.states))
+                st.json(NFA.to_json())
+
+            except Exception as e:
+                st.error(f"Error al generar el autómata: {e}")
+
+if __name__ == "__main__":
+    main()
 
